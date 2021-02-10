30ec5d570d9bbfa2f3488785afc3ed63339849b7
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-02-10 15:44:50 +0900

    作成画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,23 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to "/tasks"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
