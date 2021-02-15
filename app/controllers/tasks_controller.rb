e2cd7b4d8fdb97d9f817ea8e10cc67466d9f36c9
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-02-15 11:43:36 +0900

    %記法の追記

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update]
+  before_action :set_task, only: %i(show edit update)
   
   def index
     @tasks = Task.all
