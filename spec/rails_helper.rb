a2354be86d07942d9f3c9fdd69012bc083a8c42d
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-02-21 23:57:29 +0900

    database_rewinderの導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,14 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+  config.before(:suite) do
+    DatabaseRewinder.clean_all
+  end
+
+  config.after(:each) do
+    DatabaseRewinder.clean
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
