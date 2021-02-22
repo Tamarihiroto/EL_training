cb878a0bfce8ce8b01a3f51a92e7f049ddb0e6e9
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-02-22 14:16:19 +0900

    テストデータの変更

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
-  sequence :task_id do |i|
-    "500#{i}"
+  sequence :task_title do |i|
+    "title#{i}"
   end
-    
+
   factory :task do
-    id { generate :task_id }
     title { "title" }
     content { 'content' }
   end
