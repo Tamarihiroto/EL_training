f31c9feb539f783f3a1913c007b0d4546212d9de
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-02-24 16:19:32 +0900

    検索機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,6 +44,15 @@ class TasksController < ApplicationController
     end
   end
 
+  
+  def search
+    if params[:title].present?
+      @tasks = Task.where('title LIKE ?', "%#{params[:title]}%")
+    else
+      @tasks = Task.none
+    end
+  end
+
   private
 
   def set_task
