e1b2f1be91ee65a81c117623e69ff27cc0d877bc
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-02-24 13:26:38 +0900

    文言の変更

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  describe "降順確認" do
+  describe "タスクのテスト" do
     let(:tasks) { create_list(:task, 3) }
 
     before do 
       tasks
     end
     
-    it do
+    it "created_atの降順になること" do
       tasks_expect = tasks.reverse
       visit tasks_path
       task_list = all('.task_id')
