196d00ab81d972b5ae5de7e64d8b52636dbb11b9
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-03-01 13:16:23 +0900

    シングルクオートに変更、describeの文言の変更

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  describe "タスクのテスト" do
+  describe 'ソート' do
     let(:tasks) { create_list(:task, 3) }
 
     before do 
       tasks
     end
     
-    it "created_atの降順になること" do
+    it 'created_atの降順になること' do
       tasks_expect = tasks.reverse
       visit tasks_path
       # 2回でcreated_atで降順化
-      click_on "タイトル"
-      click_on "タイトル"
+      click_on 'タイトル'
+      click_on 'タイトル'
       task_list = all('.task_id')
       expect(task_list[0]).to have_content tasks_expect[0].id
       expect(task_list[1]).to have_content tasks_expect[1].id
       expect(task_list[2]).to have_content tasks_expect[2].id
     end
 
-    it "deadlineの昇順になること" do
+    it 'deadlineの昇順になること' do
       visit tasks_path
       # deadlineで昇順化
-      click_on "終了期限"
+      click_on '終了期限'
       task_list = all('.task_deadline')
       expect(task_list[0]).to have_content I18n.l(tasks[0].deadline, format: :short)
       expect(task_list[1]).to have_content I18n.l(tasks[1].deadline, format: :short)
