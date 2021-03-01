87fc5bc613ce8b4b809c3da5d48e78d93f3b9b74
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-03-01 15:21:26 +0900

    ページネーションのテスト

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -17,4 +17,54 @@ RSpec.describe 'Tasks', type: :system do
       expect(task_list[2]).to have_content tasks_expect[2].id
     end
   end
+  describe 'ページネーション' do
+    let(:tasks) { create_list(:task, 26) }
+
+    before do 
+      tasks
+    end
+    
+    it '1ページで25個まで表示されること' do
+      visit tasks_path
+      expect(page).to have_content tasks[0].id
+      expect(page).to have_content tasks[24].id
+      expect(page).not_to have_content tasks[25].id
+    end
+
+    it '26個から次のページで表示されること' do
+      visit tasks_path
+      click_on '次へ'
+      expect(page).not_to have_content tasks[24].id
+      expect(page).to have_content tasks[25].id
+    end
+
+    it '「前に」を押すと前のページがで表示されること' do
+      visit tasks_path
+      click_on '次へ'
+      expect(page).not_to have_content tasks[24].id
+      expect(page).to have_content tasks[25].id
+      click_on '前へ'
+      expect(page).to have_content tasks[0].id
+      expect(page).to have_content tasks[24].id
+      expect(page).not_to have_content tasks[25].id
+    end
+
+    it '最後のページが表示されること' do
+      visit tasks_path
+      click_on '最後へ'
+      expect(page).not_to have_content tasks[24].id
+      expect(page).to have_content tasks[25].id
+    end
+
+    it '最後のページが表示されること' do
+      visit tasks_path
+      click_on '最後へ'
+      expect(page).not_to have_content tasks[24].id
+      expect(page).to have_content tasks[25].id
+      click_on '最初に戻る'
+      expect(page).to have_content tasks[0].id
+      expect(page).to have_content tasks[24].id
+      expect(page).not_to have_content tasks[25].id
+    end
+  end
 end
