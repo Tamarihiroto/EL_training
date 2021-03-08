2e99766ca45c40c9e15961caaa24b8f9fdf70663
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-03-08 11:53:51 +0900

    scopeの書き方の変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, length: { maximum: 280 }
   
-  scope :sorted, lambda { |sort_params|
-    order("#{sort_params[:column]} #{sort_params[:direction]}")
-  }
+  scope :sorted, -> (sort_params) { order("#{sort_params[:column]} #{sort_params[:direction]}") }
   scope :recent, -> { order(created_at: :desc) }
 
   scope :search_title, -> (title) { where('title LIKE ?', "%#{title}%") }
