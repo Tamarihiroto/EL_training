7a934171992e27268830eeae1ca5209ddd7dde46
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-03-09 00:15:50 +0900

    undefineの削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   enum status: { default: 0, untouched: 1, in_progress: 2, done: 3 }
-  enum priority: { high: 0, middle: 1, low: 2, undefine: 3 }
+  enum priority: { high: 0, middle: 1, low: 2 }
 
   validates :title, presence: true
   validates :content, length: { maximum: 280 }
