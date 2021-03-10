35e5e377573e5e1067c7aaa9149b259a89f1a8af
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-03-10 13:44:40 +0900

    {}内のスペース追加

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,5 @@
 module TasksHelper
   def ja_select(columns, column)
-    columns.keys.map {|k| [I18n.t("enums.task.#{column}.#{k}"), k]}
+    columns.keys.map { |k| [I18n.t("enums.task.#{column}.#{k}"), k] }
   end
 end
