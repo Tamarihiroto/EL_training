5cc47a0bbb38c3786d114a0e62703289731d7b23
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-04-05 17:53:25 +0900

    ステータスのdefaultの削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  enum status: { default: 0, untouched: 1, in_progress: 2, done: 3 }
+  enum status: { untouched: 0, in_progress: 1, done: 2 }
   enum priority: { high: 0, middle: 1, low: 2 }
 
   validates :title, presence: true
