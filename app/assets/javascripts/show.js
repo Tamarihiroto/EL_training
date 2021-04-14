09f944cad4cf2bc3439758297c7d882f41fa600a
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-04-14 18:06:17 +0900

    インデントの修正

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,31 +1,31 @@
 // 完了ボタンを表示 start
 // 完了ボタンが表示する
 function showDoneButton(idName) {
-    document.getElementById(idName).style.display = 'block';
-  }
-  
-  // 内容が変更されると完了ボタンが表示
-  document.getElementById('task_content').onchange = function() {
-    showDoneButton('tasks-form-button')
-  }
-  
-  // タイトルが変更されると完了ボタンが表示
-  document.getElementById('task_title').onchange = function() {
-    showDoneButton('tasks-form-button')
-  }
-  
-  // 期限が変更されると完了ボタンが表示
-  document.getElementById('task_deadline').onchange = function() {
-    showDoneButton('tasks-form-button')
-  }
-  
-  // 優先順位が変更されると完了ボタンが表示
-  document.getElementById('task_priority').onchange = function() {
-    showDoneButton('tasks-form-button')
-  }
-  
-  // ステータスが変更されると完了ボタンが表示
-  document.getElementById('task_status').onchange = function() {
-    showDoneButton('tasks-form-button')
-  }
-  // 完了ボタンを表示 end
+  document.getElementById(idName).style.display = 'block';
+}
+
+// 内容が変更されると完了ボタンが表示
+document.getElementById('task_content').onchange = function() {
+  showDoneButton('tasks-form-button')
+}
+
+// タイトルが変更されると完了ボタンが表示
+document.getElementById('task_title').onchange = function() {
+  showDoneButton('tasks-form-button')
+}
+
+// 期限が変更されると完了ボタンが表示
+document.getElementById('task_deadline').onchange = function() {
+  showDoneButton('tasks-form-button')
+}
+
+// 優先順位が変更されると完了ボタンが表示
+document.getElementById('task_priority').onchange = function() {
+  showDoneButton('tasks-form-button')
+}
+
+// ステータスが変更されると完了ボタンが表示
+document.getElementById('task_status').onchange = function() {
+  showDoneButton('tasks-form-button')
+}
+// 完了ボタンを表示 end
