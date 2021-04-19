fd97db1d81ef3b98cfa3e1db9027b5d6c23ab439
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-04-19 14:31:20 +0900

    controllerの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(show edit update destroy)
+  # before_action :ensure_admin_user, only: %i(index edit)
+
   def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def ensure_admin_user
+    if @current_user.admin == false
+      redirect_to tasks_path
+    end
   end
 end
