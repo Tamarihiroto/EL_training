a9fb75b3ef80ade136f9721205913df1a13283a2
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-04-21 15:04:35 +0900

    controllerの作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(mail: params[:session][:mail].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def delete
+    log_out if logged_in?
+    redirect_to login_path
+  end
+end
