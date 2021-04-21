e4e82ed20d4298fd0d263025ce14ac4fe75bb2e1
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-04-21 10:24:31 +0900

    adminにnul: falseを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_083405) do
+ActiveRecord::Schema.define(version: 2021_04_21_012222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_083405) do
     t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
