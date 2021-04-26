1329648100698a9bc764b007be4340318b7cba23
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-04-26 16:53:25 +0900

    一意性の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :mail, presence: true
+  validates :mail, presence: true, uniqueness: true
   validates :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_030312) do
+ActiveRecord::Schema.define(version: 2021_04_26_075148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_26_030312) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
     t.string "password_digest", null: false
+    t.index ["mail"], name: "index_users_on_mail", unique: true
   end
 
 end
