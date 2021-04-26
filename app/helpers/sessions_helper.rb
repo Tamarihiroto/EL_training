3345c233b7a976ed6eabe109fa06439a7cdd143c
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-04-26 14:58:43 +0900

    ヘルパーの変更

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,4 @@
 module SessionsHelper
-  ログイン
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -9,14 +8,14 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
