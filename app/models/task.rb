1785cacf97b78cbc1db097cd7d644f7c3e18ecfd
Tamari Hiroto <h.tamari@buysell-technologies.com>
2021-04-28 10:14:37 +0900

    scopeを使いモジュール化

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,9 @@ class Task < ApplicationRecord
 
   validates :title, presence: true
   validates :content, length: { maximum: 280 }
-  
+
+  scope :find_current_user, -> (current_user_id) { where(user_id: current_user_id) }
+
   scope :sorted, -> (sort_params) { order("#{sort_params[:column]} #{sort_params[:direction]}") }
   scope :recent, -> { order(created_at: :desc) }
 
